71613440c4824bc7c5a25f4de44ba91c0fa25a10
felipeflourwears <A01274880@tec.mx>
2023-10-04 14:13:20 -0600

    Hello world

---
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.helloworld
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.helloworld.ui.theme.HelloWorldTheme
+import com.robotemi.sdk.Robot
+import com.robotemi.sdk.TtsRequest
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HelloWorldTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+
+        // Inicializa el SDK de Temi
+        val robot = Robot.getInstance()
+
+        // Hacer que el robot diga "Hello world"
+        val ttsRequest = TtsRequest.create("Hello world", true)
+        robot.speak(ttsRequest)
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HelloWorldTheme {
+        Greeting("Android")
+    }
+}
